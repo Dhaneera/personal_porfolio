d2d857e96869164ccbb5b89608eccafbe53b410e
Sehan-cypso <shehan@cypsolabs.com>
2024-09-22 00:05:14 +0530

    header design implemted for light mode

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,8 +13,14 @@ export default function Header({ links }: HeaderProps) {
     return (
         <header className=" hidden md:flex items-center
         justify-center fixed z-[999] w-full mt-4">
-            <div>
-                <ul>
+            <div className=" flex p-1 rounded-none border border-white
+            border-opacity-40 bg-white bg-opacity-80 shadow-lg
+            shadow-black/[0.03] backdrop-blur-[0.5rem]
+             sm:rounded-full dark:bg-gray-950  dark:border-black/40
+             dark:bg-opacity-75">
+                <ul className=" flex flex-wrap items-center
+                justify-center gap-y-1 text-[0.9rem] font-medium
+                text-gray-950 gap-10 p-3">
                     {links.map((link) => (
                         <li>
                             <NextLink
